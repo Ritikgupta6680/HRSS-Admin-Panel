a49b73d5e6527c84a7f3190ffb3ba5bf20ef4c22
Ritikgupta6680 <ritikgupta.rg.1604@gmail.com>
2026-08-06 13:40:43 +0530

    api fixed

--- src/app/services/calls/call-service.ts
+++ src/app/services/calls/call-service.ts
@@ -45,8 +45,6 @@ export class CallService {
   }
 
   private url(path: string): string {
-    console.log('apiUrl:', environment.apiUrl);
-    console.log('finalUrl:', `${environment.apiUrl}${path.startsWith('/') ? path : `/${path}`}`);
     return `${environment.apiUrl}${path.startsWith('/') ? path : `/${path}`}`;
   }
 
